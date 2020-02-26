4766ceb69b01abda10e77daccfb8c4eab347a076
Kritsada Klaimak <49706091+ksdaklmk@users.noreply.github.com>
2020-02-26 21:37:12 +0700

    Create one.py

---
+++ one.py
@@ -0,0 +1,18 @@
+class Car():
+	
+	def __init__(self, maker, year):
+		self.maker = maker
+		self.year = year
+		self.status = 'dirty'
+		
+	def clean(self):
+		print('... cleaning the car! ...')
+		self.status = 'clean'
+		
+
+my_car = Car('Honda', 2020)
+your_car = Car('Mazda', 2019)
+print(my_car.maker)
+print(f'The car is in {my_car.status} status')
+my_car.clean()
+print(f'The car is now in {my_car.status} status')

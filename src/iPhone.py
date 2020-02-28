b6ed31e777425e2e548b5922b0d67689dbc47284
Kritsada Klaimak <49706091+ksdaklmk@users.noreply.github.com>
2020-02-28 14:31:30 +0700

    Add files via upload

---
+++ src/iPhone.py
@@ -0,0 +1,29 @@
+class iPhone:
+
+    iPhone_list = ['8s', 'X', 'XS', '11']
+
+    def __init__(self, model):
+        self.model = model
+    
+    @classmethod
+    def from_year(cls, year):
+        return cls(iPhone.iPhone_list[int(year) - 2008])
+    
+    @staticmethod
+    def newest_model():
+        return iPhone.iPhone_list[len(iPhone.iPhone_list) - 1]
+    
+    def __repr__(self):
+        return self.model
+
+
+if __name__ == "__main__":
+    a = iPhone('X')
+    b = iPhone.from_year(2008)
+
+    print("Newest model: " + iPhone.newest_model())
+    print("a has " + str(a))
+    print("b has " + str(b))
+    print("a == b: " + str(a == b))
+    print("a.model == b.model: " + str(a.model == b.model))
+    
